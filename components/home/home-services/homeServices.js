4ac908eb719eea369480498a09ee733128e82ea5
Andres Mejias <aemabit@gmail.com>
2020-05-09 15:09:46 -0400

    our formula

---
+++ components/home/home-services/homeServices.js
@@ -0,0 +1,55 @@
+import "../home-services/homeServices.scss";
+
+export default function HomeServices() {
+  return (
+    <div className="wrapper-our-formula">
+      <div className="background-wrapper"></div>
+      <div className="head-formula">
+        <div>
+          <h4>OUR FORMULA</h4>
+          <h2>Development process</h2>
+        </div>
+        <div>
+          <h5>
+            View Services <span> > </span>
+          </h5>
+        </div>
+      </div>
+      <div className="wrapper-steps">
+        <div className="steps">
+          <div className="step-line">
+              <p>1</p>
+              <div className="line-inline"></div>
+          </div>
+          <h4>Client Meeting</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elitsed do e amet
+            consectetur adipiscing eiusmodua ciolo cadente.
+          </p>
+        </div>
+        <div className="steps">
+          <div className="step-line">
+              <p>2</p>
+              <div className="line-inline"></div>
+          </div>
+          <h4>Quotation</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elitsed do e amet
+            consectetur adipiscing eiusmodua ciolo cadente.
+          </p>
+        </div>
+        <div className="steps">
+          <div className="step-line">
+              <p>3</p>
+              <div className="line-inline"></div>
+          </div>
+          <h4>Initialization</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elitsed do e amet
+            consectetur adipiscing eiusmodua ciolo cadente.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import HomeHead from "../components/home/home-head/homeHead";
 import HomeMiddle from "../components/home/home-middle/homeMiddle";
+import HomeServices from "../components/home/home-services/homeServices"
 export default function Home() {
   return (
     <>
       <HomeHead />
       <HomeMiddle />
+      <HomeServices />
     </>
   );
 }
