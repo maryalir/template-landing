bdbeaed87f1d755fe59107049955eecafd0a6196
Andres Mejias <aemabit@gmail.com>
2020-11-12 10:48:43 -0500

    index

--- components/_App/layout.js
+++ components/_App/layout.js
@@ -10,6 +10,24 @@ function Layout({ children }) {
         <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
         <script type="text/javascript" src="scripts/scrollNav.js"></script>
         <title>Mia Plus +</title>
+        
+        <meta
+          name="description"
+          content="MiaPlus+ offers Marketplace insurance plans with different coverage and premium levels. Learn more about our affordable healthcare plans and compare each..."
+        />
+
+        <meta property="og:title" content="Gold Tree by CS" />
+        <meta
+          property="og:description"
+          content="MiaPlus+ offers Marketplace insurance plans with different coverage and premium levels. Learn more about our affordable healthcare plans and compare each..."
+        />
+        <meta
+          property="og:image"
+          content="https://aemabitfolder.sfo2.digitaloceanspaces.com/MIAPLUS/LogoMIAPLUSazulMINI.png"
+        />
+        <meta property="og:url" content="https://miaplus.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+
         <link rel="icon" type="image/png" href="favicon.png" />
       </Head>
       <Navigation />
